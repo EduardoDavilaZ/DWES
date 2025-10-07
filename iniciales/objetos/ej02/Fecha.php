c0c34ada4a70acfd36d8b0ecce56fd9255dd901c
Eduardo <eduardosebastiandavilazevallos.guadalupe@alumnado.fundacionloyola.net>
2025-10-07 12:50:00 +0200

    objetos v2

--- iniciales/objetos/ej02/Fecha.php
+++ iniciales/objetos/ej02/Fecha.php
@@ -19,27 +19,26 @@
         ];
 
         public function __construct($fecha){
-            /**
-             * Recoger los valores del string $fecha a las variables dd, mm, aa y convertirlos a int.
-             */
-            $this->dd = (int)(substr($fecha, 0, 2));
-            $this->mm = (int)(substr($fecha, 3, 2));
-            $this->aa = (int)(substr($fecha, 6, 4));
+            /* Recoger los valores del string $fecha a las variables dd, mm, aa y convertirlos a int. */
+            /* Se usó explode porque al usar substr no funcionaría correctamente al introducir 1 dígito por campo. */
+            $fecha = explode("-", $fecha);
+            $this->dd = (int)($fecha[0]);
+            $this->mm = (int)($fecha[1]);
+            $this->aa = (int)($fecha[2]);
 
-            
-            $this->esBisiesto(); /* Modificar el array si es Bisiesto */
+            /* Modificar el array si es Bisiesto */
+            $this->esBisiesto(); 
         }
 
-        public function esBisiesto(){
+        /**
+         * Función privada que se llama desde el constructor para cambiar el número de días 
+         * de febrero si el año es Bisiesto.
+         */
+        private function esBisiesto(){
             if (($this->aa % 4 == 0 && $this->aa % 100 != 0) || $this->aa % 400 == 0){
-                /**
-                 * Cambiar el número de días de febrero cada vez que se consulte si el año es Bisiesto
-                 */
                 $this->datos[2][1] = 29;
-                return true;
             } else {
                 $this->datos[2][1] = 28;
-                return false;
             }
         }
         
@@ -52,8 +51,8 @@
         }
 
         /**
-         * Se consulta directamente al array porque si es
-         * bisiesto, ya habrá sido cambiado en el constructor.
+         * Se consulta directamente al array porque si es bisiesto, ya habrá sido cambiado 
+         * desde el constructor.
          */
         public function diasDelMes(){
             return $this->datos[$this->mm][1];
--- iniciales/objetos/ej02/index.php
+++ iniciales/objetos/ej02/index.php
@@ -1,23 +1,15 @@
 <?php 
     include 'Fecha.php';
 
-    var_dump($_GET);
-
+    /* Al enviar, el resultado se muestra desde la página principal. */
     if (isset($_GET["enviar"])){
         $fecha = $_GET["fecha"];
         visualizar($fecha);
     }
 
     function visualizar($fecha){
-
         /* Pasar un string de la fecha al constructor */
         $objFecha = new Fecha($fecha);
-
-        if ($objFecha->esBisiesto()){
-            echo "<h3>El año bisiesto </h3>";
-        } else {
-            echo "<h3>El año NO es bisiesto </h3>";
-        }
 
         /* Visualizar la fecha en el formato dd/Mes/aaaa. */
         echo '<h3>' . $objFecha->toString() . '</h3>';
@@ -25,26 +17,26 @@
         /* Visualizar los días del mes. */
         echo '<h3>El mes ' . $objFecha->getMes() . ' tiene ' . $objFecha->diasDelMes() . ' días.</h3>';
     }
-
 ?>
 
 <!DOCTYPE html>
 <html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <title>Document</title>
-</head>
-<body>
-    <form action="index.php" method="GET">
-        <label>Fecha (dd-mm-aaaa): </label>
-        <input type="text" name="fecha" id="fecha">
-        <input type="submit" value="enviar" name="enviar">
-    </form>
+    <head>
+        <meta charset="UTF-8">
+        <title>Document</title>
+    </head>
+    <body>
+        <form action="index.php" method="GET">
+            <label>Fecha (dd-mm-aaaa): </label>
+            <input type="text" name="fecha" id="fecha">
+            <input type="submit" value="enviar" name="enviar">
+        </form>
 
-    <?php
-        if (isset($_GET["enviar"])){
-            echo '<h1>Mostrar = ' . $fecha . '</h1>';
-        }
-    ?>
-</body>
+        <?php
+            /* Mostrar la fecha introducida al enviar */
+            if (isset($_GET["enviar"])){
+                echo '<h1>Mostrar = ' . $fecha . '</h1>';
+            }
+        ?>
+    </body>
 </html>
