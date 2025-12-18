503636fa41538647d39093a6f498652650a08c5e
Eduardo Davila <eduardosebastiandavilazevallos.guadalupe@alumnado.fundacionloyola.net>
2025-12-18 10:08:13 +0100

    pruebas consultas preparadas

--- InyeccionSQL/view/inicioSesion.php
+++ InyeccionSQL/view/inicioSesion.php
@@ -7,13 +7,38 @@
     <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
-    <form action="./index.php?c=Usuario&m=inicioSesion" method="POST">
+    <div>
+        <button type="button" id="btnInseguro">Inicio de sesión inseguro</button>
+    </div>
+    <form action="./index.php?c=Usuario&m=inicioSesion" id="form" method="POST">
         <h1>Inicio de sesión</h1>
         <label for="correo">Correo: </label>
         <input type="text" name="correo" placeholder="Introduce tu correo">
         <label for="pw">Contraseña: </label>
         <input type="password" name="pw" placeholder="Introduce tu contraseña">
-        <input type="submit" name="Enviar">
+        <input type="submit" name="Enviar" id="btnEnviar">
     </form>
+
+    <script>
+        const btn = document.getElementById('btnInseguro');
+        const form = document.getElementById('form');
+        const submit = document.getElementById('btnEnviar');
+
+        let inseguro = false;
+
+        btn.addEventListener('click', () => {
+            inseguro = !inseguro;
+
+            if (inseguro) {
+                form.action = './index.php?c=Usuario&m=inicioSesionInseguro';
+                submit.value = 'Inicio de sesión inseguro';
+                btn.textContent = 'Cambiar a inicio de sesión seguro';
+            } else {
+                form.action = './index.php?c=Usuario&m=inicioSesion';
+                submit.value = 'Inicio de sesión';
+                btn.textContent = 'Cambiar a inicio de sesión inseguro';
+            }
+        });
+    </script>
 </body>
 </html>
